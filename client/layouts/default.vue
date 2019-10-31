644e783b0d01b7da72c798aa633f6d89cc1d996f
samanar <ahmadianrad.saman@gmail.com>
2019-10-31 10:02:58 +0330

    initial commit

---
+++ client/layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"/>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      fixed
+      dense
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title v-text="title"/>
+      <v-spacer/>
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content class="content_background">
+      <v-container>
+        <nuxt/>
+      </v-container>
+    </v-content>
+    <v-footer
+      fixed
+      app
+    >
+      <span>&copy; copy right stuff here</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer: false,
+                items: [
+                    {
+                        icon: 'mdi-apps',
+                        title: 'Welcome',
+                        to: '/'
+                    },
+                    {
+                        icon: 'mdi-chart-bubble',
+                        title: 'Inspire',
+                        to: '/inspire'
+                    }
+                ],
+                title: 'Samanar'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .content_background {
+    background-color: #f0f0f0;
+  }
+</style>
