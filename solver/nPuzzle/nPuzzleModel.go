37fd8f488d9c63728d2f1f573de5bdd179785ac3
samanar <ahmadianrad.saman@gmail.com>
2019-11-01 17:55:03 +0330

    node and npuzzle added

--- solver/nPuzzle/nPuzzleModel.go
+++ solver/nPuzzle/nPuzzleModel.go
@@ -1,24 +1,32 @@
 package nPuzzle
 
 type NPuzzle struct {
-	Numbers []int
 	RowSize int
 	ColSize int
-	parent *NPuzzle
-	children []*NPuzzle
+	Root    *Node
 }
 
-func (nPuzzle *NPuzzle) convertIndexToCoordinates(index int) (int , int) {
-	i :=  index / nPuzzle.RowSize
-	j :=  index - (i * nPuzzle.RowSize)
+type Node struct {
+	Numbers  []int
+	parent   *Node
+	children []*Node
+}
+
+func convertIndexToCoordinates(index int, rowSize, colSize int) (int, int) {
+	i := index / rowSize
+	j := index - (i * rowSize)
 	if j < 0 {
 		j = j * -1
 	}
-	return i , j
+	return i, j
 }
 
-func (nPuzzle *NPuzzle) findZero() int {
-	for index ,n := range nPuzzle.Numbers {
+func convertCoordinatesToIndex(i, j, rowSize int) int {
+	return i*rowSize + j
+}
+
+func (node *Node) findZero() int {
+	for index, n := range node.Numbers {
 		if n == 0 {
 			return index
 		}
@@ -26,7 +34,39 @@ func (nPuzzle *NPuzzle) findZero() int {
 	return -1
 }
 
-func (nPuzzle *NPuzzle)  GenerateChildren() {
-	zeroIndex := nPuzzle.findZero()
-	i , j := nPuzzle.convertIndexToCoordinates(zeroIndex)
+func (node *Node) availableMoves(rowSize, colSize int) (int, int, [4]bool) {
+	var moves [4]bool
+	zeroIndex := node.findZero()
+	zeroI, zeroJ := convertIndexToCoordinates(zeroIndex , rowSize , colSize)
+	if zeroI != 0 {
+		moves[0] = true
+	}
+	if zeroJ != colSize-1 {
+		moves[1] = true
+	}
+	if zeroI != rowSize-1 {
+		moves[2] = true
+	}
+	if zeroJ != 0 {
+		moves[3] = true
+	}
+
+	return zeroI, zeroJ, moves
+}
+
+func SwapNumbers(numbers *[]int, first, second int) {
+	(*numbers)[first], (*numbers)[second] = (*numbers)[second], (*numbers)[first]
+}
+
+func (node Node) generateChild(direction string, zeroI, zeroJ int) *Node {
+	numbers := node.Numbers
+	switch direction {
+	case "up":
+		//numbers
+	}
+
+}
+
+func (node *Node) GenerateChildren() {
+
 }
